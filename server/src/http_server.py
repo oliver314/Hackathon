c3796ef40138abf91d5e823a3a832bcf41d58c30
Archangel7777777 <james.rand1@aol.co.uk>
2017-11-25 23:00:12 +0000

    Updated http_server to serve all files

--- server/src/http_server.py
+++ server/src/http_server.py
@@ -6,21 +6,32 @@ class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            self.homepage()
+            if self.path == "/":
+                self.send_file("index.html", "text/html")
+            else:
+                self.send_file(self.path[1:])
+
         except Exception as e:
             print(e)
 
-    def homepage(self):
-        with open("index.html", "rb") as file:
+    def send_file(self, filename, encoding=None):
+        with open(filename, "rb") as file:
             self.send_response(200)
-            self.send_header("Content-type", "text/html")
+            if not encoding:
+                self.send_header("Content-type", encoding)
             self.end_headers()
             self.wfile.write(file.read())
 
+    def do_POST(self):
+        self.send_response(200)
+        length = int(self.headers["content-length"])
+        data = self.rfile.read(length)
+        print(data)
+        #database.insertValues(data)
 
 
 if __name__ == "__main__":
-    server = socketserver.TCPServer(("", 80), Handler)
+    server = socketserver.TCPServer(("", 8080), Handler)
     server.socket = ssl.wrap_socket(
         server.socket,
         certfile="cert.pem",
