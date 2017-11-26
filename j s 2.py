9f3dfb928eef43e225749d5f1e9c82a6dea4ad3e
Archangel7777777 <james.rand1@aol.co.uk>
2017-11-26 10:41:00 +0000

    Fixes syntax error on the server

--- j s 2.py
+++ j s 2.py
@@ -108,9 +108,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         self.send_response(200)
         length = int(self.headers["content-length"])
-        if self.path == "/mbed":
 
-        print('Hello')
         data = self.rfile.read(length)
         print(data)
         json_data = json.loads(data)
