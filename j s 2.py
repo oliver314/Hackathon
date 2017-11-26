8082c00d62df0bc23461bd8de16b8faf88a085f7
SK Nguyen <sykhanh1997@gmail.com>
2017-11-26 10:39:04 +0000

    1038am commit

--- j s 2.py
+++ j s 2.py
@@ -99,4 +99,4 @@ if __name__ == "__main__":
         try:
             httpd.serve_forever()
         except KeyboardInterrupt:
-            print(" Recieved Shutting Down")
+            print(" Received Shutting Down")
